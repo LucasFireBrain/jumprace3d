733bf370ef0afa7d0ca5169029f16e8edf48eedf
Lucas <lucasperaltafuret@gmail.com>
2021-08-11 00:48:13 -0400

    Version 0.0.0 Initial commit

---
+++ Assets/Scripts/Platform/Platform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected virtual void Bounce(Player player) {
+        player.BounceUp();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.rigidbody.GetComponent<Player>() != null) { 
+            Bounce(collision.rigidbody.GetComponent<Player>());
+        }
+    }
+}
